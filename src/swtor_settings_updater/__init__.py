ad8ccb81927fd4536f7ae419377ea6ff3bc08ba1
Johan Kiviniemi <devel@johan.kiviniemi.name>
2020-07-30 01:11:25 +0300

    Release v0.0.2
    * Add CHANGELOG.md

--- src/swtor_settings_updater/__init__.py
+++ src/swtor_settings_updater/__init__.py
@@ -2,4 +2,4 @@ from swtor_settings_updater.character import Character, SETTINGS_DIR
 from swtor_settings_updater.chat import Chat
 from swtor_settings_updater.color import Color
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
