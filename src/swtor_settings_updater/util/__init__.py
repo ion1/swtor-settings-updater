4f64183ef14ca0c11a5d76bdf5f48477012912b2
Johan Kiviniemi <devel@johan.kiviniemi.name>
2020-08-22 17:07:48 +0300

    swtor_settings_updater.util: Fix __all__ to consist of strings

--- src/swtor_settings_updater/util/__init__.py
+++ src/swtor_settings_updater/util/__init__.py
@@ -3,9 +3,9 @@ from .option_transformer import OptionTransformer
 from .swtor_case import swtor_lower, swtor_upper
 
 __all__ = [
-    CP1252_PRINTABLE,
-    regex_character_class,
-    OptionTransformer,
-    swtor_lower,
-    swtor_upper,
+    "CP1252_PRINTABLE",
+    "regex_character_class",
+    "OptionTransformer",
+    "swtor_lower",
+    "swtor_upper",
 ]
