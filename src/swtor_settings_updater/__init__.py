614d9c80b8f221fb701c40c4b2b69d330a558b8c
Johan Kiviniemi <devel@johan.kiviniemi.name>
2020-08-22 16:37:11 +0300

    swtor_settings_updater: Re-export .character.CharacterMetadata

--- src/swtor_settings_updater/__init__.py
+++ src/swtor_settings_updater/__init__.py
@@ -1,7 +1,7 @@
-from .character import Character, default_settings_dir
+from .character import Character, CharacterMetadata, default_settings_dir
 from .chat import Chat
 from .color import Color
 
 __version__ = "0.0.3"
 
-__all__ = ["Character", "default_settings_dir", "Chat", "Color"]
+__all__ = ["Character", "CharacterMetadata", "default_settings_dir", "Chat", "Color"]
